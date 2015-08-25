e17ad0f99d749e1ace50c6f9cce1fdf8e4166b5c
Antonia Blair <antoniablair@gmail.com>
2015-08-24 23:32:15 -0400

    Updates initial route

--- curlyapp/curlyapp/routes/index.js
+++ curlyapp/curlyapp/routes/index.js
@@ -6,4 +6,23 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Curly or Straight?' });
 });
 
+router.post('/', function(req, res, next){
+	res.render('forecast', {title: 'Your Curly Forecast'});
+});
+
+// exports.index = function(req, res){
+//   res.render('index', { title: 'Here comes your results' });
+// };
+
+exports.getWeather = function(req, res){
+    var cityName = req.body.txtCity;
+    weather.getWeather(cityName,function(data){
+        if(data.status){
+          data = JSON.parse(data.info);
+          console.log(data);
+          res.render('forecast');
+        }
+      });
+};
+
 module.exports = router;
